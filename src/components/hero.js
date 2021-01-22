f24acf9d5c854a8d9f2951b9c599076582e2a8d8
Håvard Holen <havardholen@Havards-MacBook-Pro.local>
2021-01-22 15:22:32 +0100

    animation tweak

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,6 +2,7 @@ import React from "react"
 import { cn } from "../helpers"
 import MyAnimation from "./myanimation"
 import styles from "./utils.module.css"
+import waves from "../css/waves.module.css"
 const hero = () => {
   return (
     <div className={styles.heroContainer}>
@@ -9,6 +10,9 @@ const hero = () => {
         <div className={cn(styles.heroContent)}>
           <MyAnimation />
           <div className={styles.heroOverlay}></div>
+          {/* <div className={waves.wave}></div> */}
+          {/* <div className={cn(waves.wave, waves.waveLeft)}></div> */}
+          {/* <div className={cn(waves.wave, waves.waveRight)}></div> */}
         </div>
       </div>
     </div>
